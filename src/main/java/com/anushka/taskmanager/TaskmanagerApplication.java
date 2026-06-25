f394daec8c8eeb4a00d01cda9e9c8d68eb4f5c04
Anushka Bhandari <anushka.bhandari2023@vitstudent.ac.in>
2026-06-25 16:23:21 +0530

    fix: resolve TaskmanagerApplication merge markers and enable JPA auditing

--- src/main/java/com/anushka/taskmanager/TaskmanagerApplication.java
+++ src/main/java/com/anushka/taskmanager/TaskmanagerApplication.java
@@ -2,15 +2,13 @@ package com.anushka.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TaskmanagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaskmanagerApplication.class, args);
-	}
-<<<<<<< HEAD
-=======
-
->>>>>>> 438f23f07e9de93a71c7682bf098a97a3f854a55
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagerApplication.class, args);
+    }
 }
