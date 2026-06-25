48c15c009c57cf70fa66660d895254c9958cc4c9
Anushka Bhandari <anushka.bhandari2023@vitstudent.ac.in>
2026-06-25 16:29:11 +0530

    refactor: make UserRepository.findByEmail return Optional

--- src/main/java/com/anushka/taskmanager/repository/UserRepository.java
+++ src/main/java/com/anushka/taskmanager/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.anushka.taskmanager.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.anushka.taskmanager.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
